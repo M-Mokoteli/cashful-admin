b0970c491218e94ca5be4c0e7f73d19bf8ad16d9
M-Mokoteli <63512196+M-Mokoteli@users.noreply.github.com>
2022-12-08 20:19:38 +0200

    Update DocBox.tsx

--- src/components/pages/accounts/DocBox.tsx
+++ src/components/pages/accounts/DocBox.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Button from '../../layout/form/Button'
-import { UserDoc } from '../../../utils/interface/Models'
-import { updateDoc } from 'firebase/firestore'
+import { LoanRequest, STATUS, UserDoc } from '../../../utils/interface/Models'
+import { QuerySnapshot, updateDoc, where } from 'firebase/firestore'
 import { Collections } from '../../../utils/firebase/Collections'
 import { createDoc } from '../../../utils/firebase/config'
+import { StateContext } from "../../../utils/context/MainContext";
+import { initLoadData, URHpopulateData } from '../home/HomeUtils';
 
 interface iDocBox {
     id: string
@@ -13,7 +15,11 @@ interface iDocBox {
     isPdf?: boolean
 }
 export default function DocBox({ id, status, url, infoKey, isPdf = false }: iDocBox) {
-
+    const { levels } = useContext(StateContext);
+    const [requests, setRequests] = useState<LoanRequest[]>([]);
+      const populateData = async (data: QuerySnapshot<LoanRequest>) => {
+        URHpopulateData(data, levels, setRequests);
+      };
     const onUpdateStatus = async (_status: "rejected" | "approved") => {
         //send to upcoming..
         const yes = confirm("Are you sure you want to change the status to " + _status + "?")
@@ -36,8 +42,18 @@ export default function DocBox({ id, status, url, infoKey, isPdf = false }: iDoc
             <div className='flex flex-col gap-2'>
                 <Button onClick={() => { onUpdateStatus("approved") }}>Approve</Button>
                 <Button seconday onClick={() => { onUpdateStatus("rejected") }}>Reject</Button>
+                <label className="containers">
+  <input type="radio"  name="radio"/>
+  <span className="checkmark"></span>
+</label>
+<label className="containers secondConst">
+  <input type="radio"  name="radio"/>
+  <span className="checkmark"></span>
+</label>
                 <p className='text-sm text-gray-700'><b>Status</b>: {status}</p>
             </div>
+           
         </div>
+        
     )
 }
